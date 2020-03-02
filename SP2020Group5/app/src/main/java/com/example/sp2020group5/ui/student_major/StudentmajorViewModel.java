abb173762623ad9016fc71bcd74f3c15db84b35f
venkat prudhvi Dommaraju <S537236@nwmissouri.edu>
2020-03-02 17:54:34 -0600

    created a fragment for major

---
+++ SP2020Group5/app/src/main/java/com/example/sp2020group5/ui/student_major/StudentmajorViewModel.java
@@ -0,0 +1,7 @@
+package com.example.sp2020group5.ui.student_major;
+
+import androidx.lifecycle.ViewModel;
+
+public class StudentmajorViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
