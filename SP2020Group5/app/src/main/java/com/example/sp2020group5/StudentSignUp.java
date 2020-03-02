234379e601dda6aceeb7d7db110ca3773c91e4af
Bhavya-123 <S536778@nwmissouri.edu>
2020-03-02 00:39:08 -0600

    Added code for Navigation from student signup page to launch screen on click of signup button after successful

--- SP2020Group5/app/src/main/java/com/example/sp2020group5/StudentSignUp.java
+++ SP2020Group5/app/src/main/java/com/example/sp2020group5/StudentSignUp.java
@@ -11,4 +11,10 @@ public class StudentSignUp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_sign_up);
     }
+	public void onClickRegister_asstudent(View v){
+        Toast.makeText(getApplicationContext(),"Your Registration as student is successful.Please Login",Toast.LENGTH_LONG).show();
+        Intent ini=new Intent(this, LoginActivity.class);
+        setResult(RESULT_OK,ini);
+        finish();
+}
 }
