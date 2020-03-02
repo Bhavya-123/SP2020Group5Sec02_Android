a043cb193ce844840ec3fef9210225038f3b6950
Bhavya-123 <S536778@nwmissouri.edu>
2020-03-01 23:17:12 -0600

    Created a staff activity_staff_sign_up.xml and StaffSignUp.java Added the required fields for the staff signup in the xml

---
+++ SP2020Group5/app/src/main/java/com/example/sp2020group5/StaffSignUp.java
@@ -0,0 +1,20 @@
+package com.example.sp2020group5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.sp2020group5.ui.login.LoginActivity;
+
+public class StaffSignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_staff_sign_up);
+    }
+    
+}
