fa2c503f543f6cd098fa249914ca710f14051e97
venkat prudhvi Dommaraju <S537236@nwmissouri.edu>
2020-03-02 21:10:59 -0600

    added navigation to buttons in major fragment

--- SP2020Group5/app/src/main/java/com/example/sp2020group5/ui/student_major/studentmajor.java
+++ SP2020Group5/app/src/main/java/com/example/sp2020group5/ui/student_major/studentmajor.java
@@ -1,21 +1,23 @@
 package com.example.sp2020group5.ui.student_major;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.sp2020group5.JobsActivity;
 import com.example.sp2020group5.R;
 
 public class studentmajor extends Fragment {
 
+    Button button,button9,button10,button11,button12;
     private StudentmajorViewModel mViewModel;
 
     public static studentmajor newInstance() {
@@ -25,7 +27,48 @@ public class studentmajor extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.studentmajor_fragment, container, false);
+        View v= inflater.inflate(R.layout.studentmajor_fragment, container, false);
+        button = (Button) v.findViewById(R.id.button);
+        button9 = (Button) v.findViewById(R.id.button9);
+        button10 = (Button) v.findViewById(R.id.button10);
+        button11 = (Button) v.findViewById(R.id.button11);
+        button12 = (Button) v.findViewById(R.id.button12);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(getActivity(), JobsActivity.class);
+                startActivity(intent1);
+            }
+        });
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(getActivity(), JobsActivity.class);
+                startActivity(intent1);
+            }
+        });
+        button10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(getActivity(), JobsActivity.class);
+                startActivity(intent1);
+            }
+        });
+        button11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(getActivity(), JobsActivity.class);
+                startActivity(intent1);
+            }
+        });
+        button12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(getActivity(), JobsActivity.class);
+                startActivity(intent1);
+            }
+        });
+        return v;
     }
 
     @Override
--- SP2020Group5/app/src/main/java/com/example/sp2020group5/ui/student_search/searchFragment.java
+++ SP2020Group5/app/src/main/java/com/example/sp2020group5/ui/student_search/searchFragment.java
@@ -1,9 +1,11 @@
 package com.example.sp2020group5.ui.student_search;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -12,19 +14,30 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.sp2020group5.JobsActivity;
 import com.example.sp2020group5.R;
 
-public class SlideshowFragment extends Fragment {
+public class searchFragment extends Fragment {
 
-    private SlideshowViewModel slideshowViewModel;
+    private searchViewModel searchViewModel;
+
+    Button button7;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        slideshowViewModel =
-                ViewModelProviders.of(this).get(SlideshowViewModel.class);
+        searchViewModel =
+                ViewModelProviders.of(this).get(searchViewModel.class);
         View root = inflater.inflate(R.layout.student_search, container, false);
+        button7 = (Button)root.findViewById(R.id.button7);
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(getActivity(), JobsActivity.class);
+                startActivity(intent1);
+            }
+        });
         final TextView textView = root.findViewById(R.id.text_slideshow);
-        slideshowViewModel.getText().observe(this, new Observer<String>() {
+        searchViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
