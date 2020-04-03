3025cf14c85d4b9bbc26a12a5728898fba28e0b3
Dheeraj Edupuganti <dheeru998@gmail.com>
2020-04-03 15:32:27 -0500

    addded a toast message in the functionality of apply button in JobsActivity.java

--- SP2020Group5/app/src/main/java/com/example/sp2020group5/JobsActivity.java
+++ SP2020Group5/app/src/main/java/com/example/sp2020group5/JobsActivity.java
@@ -1,6 +1,9 @@
 package com.example.sp2020group5;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,13 @@ public class JobsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jobs);
+
+        Button apply = (Button) findViewById(R.id.applyBTN);
+        apply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"You Application was successfull",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
