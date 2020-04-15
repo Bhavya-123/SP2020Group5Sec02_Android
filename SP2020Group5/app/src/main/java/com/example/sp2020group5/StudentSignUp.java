c171ed18e975998f58bddefcf6bc1a07ca261b2a
venkat prudhvi Dommaraju <S537236@nwmissouri.edu>
2020-04-14 19:26:14 -0500

    Added fire base database to StudentSignUp.java for authentication

--- SP2020Group5/app/src/main/java/com/example/sp2020group5/StudentSignUp.java
+++ SP2020Group5/app/src/main/java/com/example/sp2020group5/StudentSignUp.java
@@ -1,64 +1,117 @@
 package com.example.sp2020group5;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
 
 
-public class StudentSignUp extends AppCompatActivity {
-    String fname,lname,email,password;
+public class StudentSignUp extends AppCompatActivity implements View.OnClickListener{
+    String fname,lname;
+    private EditText emailET,passwordET;
+    private Button stusignupBTN;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_sign_up);
+        emailET = (EditText) findViewById(R.id.emailET);
+        passwordET = (EditText) findViewById(R.id.passwordET);
+        stusignupBTN = (Button) findViewById(R.id.stusignupBTN);
+        stusignupBTN.setOnClickListener(this);
+        progressDialog = new ProgressDialog(this);
+        firebaseAuth = FirebaseAuth.getInstance();
+
     }
-	public void onClickRegister_asstudent(View v){
+
+    private void registerUser(){
+        String email = emailET.getText().toString().trim();
+        String password = passwordET.getText().toString().trim();
         EditText fnameET = findViewById(R.id.firstnameET);
         fname=fnameET.getText().toString();
         EditText lnameET = findViewById(R.id.lastnameET);
         lname=lnameET.getText().toString();
-        EditText emailET = findViewById(R.id.emailET);
-        email=emailET.getText().toString();
-        EditText passwordET = findViewById(R.id.passwordET);
-        password=passwordET.getText().toString();
+
         if(!(fname.isEmpty() && lname.isEmpty() && email.isEmpty() && password.isEmpty())){
             if(!(fname.length()>50)){
                 if(!(lname.length()>50)) {
                     if ((email.contains("@nwmissouri.edu") || email.contains("@NWMISSOURI.EDU"))) {
                         if (!(password.length() < 8)) {
 
-                            Toast.makeText(getApplicationContext(),"Your Registration as student is successful.Please Login",Toast.LENGTH_LONG).show();
-                            Intent ini=new Intent(this, StudentLoginActivity.class);
-                            startActivity(ini);
+//                            Toast.makeText(getApplicationContext(),"Your Registration as student is successful.Please Login",Toast.LENGTH_LONG).show();
 
                         } else {
                             Toast.makeText(getApplicationContext(), "password cannot be less than 8 characters", Toast.LENGTH_LONG).show();
+                            return;
                         }
 
                     } else {
                         Toast.makeText(getApplicationContext(), "Please enter SID in correct format(e.g. XXXXXX@nwmissouri.edu)", Toast.LENGTH_LONG).show();
+                        return;
 
                     }
                 }else{
                     Toast.makeText(getApplicationContext(),"Last Name field cannot be more than 50 characters long",Toast.LENGTH_LONG).show();
+                    return;
                 }
 
             }else{
                 Toast.makeText(getApplicationContext(),"First Name field cannot be more than 50 characters long",Toast.LENGTH_LONG).show();
+                return;
             }
 
                 }else{
             Toast.makeText(getApplicationContext(),"All fields are Mandatory.",Toast.LENGTH_LONG).show();
+            return;
 
         }
+        progressDialog.setMessage("Registering user");
+        progressDialog.show();
+        firebaseAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(StudentSignUp.this,"Registered Successfully",Toast.LENGTH_LONG).show();
+                            progressDialog.dismiss();
+                            Intent intent = new Intent(StudentSignUp.this,StudentLoginActivity.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            Toast.makeText(StudentSignUp.this,"could not register",Toast.LENGTH_SHORT).show();
+                            progressDialog.dismiss();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == stusignupBTN){
+            registerUser();
+        }
+    }
+//	public void onClickRegister_asstudent(View v){
+//
 
 
-
-}
+//}
 }
