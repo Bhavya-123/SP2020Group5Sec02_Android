6a421db16eadb40f9eb93f372966a737797d3282
venkat prudhvi Dommaraju <S537236@nwmissouri.edu>
2020-04-16 01:21:31 -0500

    updated validation for StudentLoginActivity.java(accessing login details from database)

--- SP2020Group5/app/src/main/java/com/example/sp2020group5/StudentLoginActivity.java
+++ SP2020Group5/app/src/main/java/com/example/sp2020group5/StudentLoginActivity.java
@@ -17,13 +17,21 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class StudentLoginActivity extends AppCompatActivity {
-    String emailid,studentpwd;
+    String emailid, studentpwd;
     EditText emailidET;
     EditText studentpwdET;
     Button studentloginBTN;
     FirebaseAuth firebaseAuth;
+    FirebaseDatabase root;
+    DatabaseReference reference;
     private FirebaseAuth.AuthStateListener mAuthStateListner;
 
     @Override
@@ -31,49 +39,76 @@ public class StudentLoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_login);
         emailidET = (EditText) findViewById(R.id.stuloginidET);
-        studentpwdET=(EditText) findViewById(R.id.stuloginpwdET);
+        studentpwdET = (EditText) findViewById(R.id.stuloginpwdET);
         studentloginBTN = findViewById(R.id.studentloginBTN);
-        emailid=emailidET.getText().toString();
-        studentpwd=studentpwdET.getText().toString();
+        emailid = emailidET.getText().toString();
+        studentpwd = studentpwdET.getText().toString();
         firebaseAuth = FirebaseAuth.getInstance();
 
         studentloginBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = emailidET.getText().toString().trim();
-                String password = studentpwdET.getText().toString().trim();
+                final String password = studentpwdET.getText().toString().trim();
 
-                if(TextUtils.isEmpty(email)){
-                    Toast.makeText(StudentLoginActivity.this,"Please enter email-id",Toast.LENGTH_SHORT).show();
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(StudentLoginActivity.this, "Please enter email-id", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if(TextUtils.isEmpty(password)){
-                    Toast.makeText(StudentLoginActivity.this,"Please enter password",Toast.LENGTH_SHORT).show();
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(StudentLoginActivity.this, "Please enter password", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if(password.length()<8){
-                    Toast.makeText(StudentLoginActivity.this,"Password is too short",Toast.LENGTH_SHORT).show();
+                if (password.length() < 8) {
+                    Toast.makeText(StudentLoginActivity.this, "Password is too short", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
-                firebaseAuth.signInWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(StudentLoginActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                  startActivity(new Intent(getApplicationContext(),StudentActivity.class));
+                reference = FirebaseDatabase.getInstance().getReference().child("Students");
+                Query query = reference.orderByChild("email").equalTo(email);
+                query.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists()) {
+                            for (DataSnapshot user : dataSnapshot.getChildren()) {
+                                StudentDetails stu = user.getValue(StudentDetails.class);
+                                if (stu.password.equals(password)) {
+                                    Intent intent = new Intent(StudentLoginActivity.this, StudentActivity.class);
+                                    startActivity(intent);
                                 } else {
-                                    Toast.makeText(StudentLoginActivity.this,"Login failed or User is not availabe",Toast.LENGTH_SHORT).show();
-
+                                    Toast.makeText(getApplicationContext(), "Password is wrong", Toast.LENGTH_SHORT).show();
                                 }
-
                             }
-                        });
+                        } else {
+                            Toast.makeText(getApplicationContext(), "User not found", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+//                firebaseAuth.signInWithEmailAndPassword(email, password)
+//                        .addOnCompleteListener(StudentLoginActivity.this, new OnCompleteListener<AuthResult>() {
+//                            @Override
+//                            public void onComplete(@NonNull Task<AuthResult> task) {
+//                                if (task.isSuccessful()) {
+//                                  startActivity(new Intent(getApplicationContext(),StudentActivity.class));
+//                                } else {
+//                                    Toast.makeText(StudentLoginActivity.this,"Login failed or User is not availabe",Toast.LENGTH_SHORT).show();
+//
+//                                }
+//
+//                            }
+//                        });
             }
         });
 
-
-
+    }
+}
 
 //        mAuthStateListner = new FirebaseAuth.AuthStateListener() {
 //            @Override
@@ -111,7 +146,6 @@ public class StudentLoginActivity extends AppCompatActivity {
 //        });
 
 
-    }
 
 //    @Override
 //    protected void onStart() {
@@ -145,4 +179,3 @@ public class StudentLoginActivity extends AppCompatActivity {
 //
 //    }
 
-}
