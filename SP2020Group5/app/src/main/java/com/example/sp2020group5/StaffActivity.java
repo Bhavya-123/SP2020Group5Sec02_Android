6f290e4ee1f274b82020d41c9cef5634b371bdc4
Bhavya-123 <S536778@nwmissouri.edu>
2020-04-17 21:12:52 -0500

    Modified a message in staff activity and added a validation for deadline in Post A Job menu item in Staff home.

--- SP2020Group5/app/src/main/java/com/example/sp2020group5/StaffActivity.java
+++ SP2020Group5/app/src/main/java/com/example/sp2020group5/StaffActivity.java
@@ -34,7 +34,7 @@ public class StaffActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "No messages to display", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
